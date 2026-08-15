b1f8d7eca71185ac1cd4d3affa3f9f35cccb9d25
Hammad Aslam <hammadpilot9099@gmail.com>
2026-08-15 15:22:09 +0500

    Day 31: Playwright

---
+++ playwright31.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://quotes.toscrape.com/js/")
+
+    quote = page.locator(".text").first.text_content()
+
+    print(quote)
+
+    browser.close()

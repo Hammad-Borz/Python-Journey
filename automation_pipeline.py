6f5df61abc43b7f829584a5a94dda60b030e4dea
Hammad Aslam <hammadpilot9099@gmail.com>
2026-08-16 15:16:26 +0500

    Day 36: Project: Automation Pipeline

---
+++ automation_pipeline.py
@@ -0,0 +1,39 @@
+import requests
+from dotenv import load_dotenv
+import os
+from google import genai
+
+# Load existing Gemini API key
+load_dotenv("chat_with_docs/.env")
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+print(api_key is not None)
+
+# Get data from GitHub API
+response = requests.get("https://api.github.com")
+
+data = response.json()
+
+# Extract useful information
+print(data["current_user_url"])
+print(data["repository_search_url"])
+
+info = {
+    "user_url": data["current_user_url"],
+    "repository_search": data["repository_search_url"]
+}
+
+print(info)
+
+# Send data to Gemini
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=f"Briefly summarize this data: {info}"
+)
+
+print(response.text)
+with open("ai_result.txt", "w", encoding="utf-8") as file:
+    file.write(response.text)
